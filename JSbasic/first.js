1eedb0c49415720a4a7dfd9fea2978d628606ce9
Chi-Honey <necktop9711@gmail.com>
2022-04-17 11:50:47 +0900

    Daily study 220417

---
+++ JSbasic/first.js
@@ -0,0 +1,19 @@
+let a = 5;
+let b = 7;
+console.log(a * b);
+console.log(typeof a);
+
+//한행주석
+/**
+ * 여러행으로 감싼 주석
+ */
+
+// 하나의 const, let로 여러개의 변수를 선언할 수 있다.
+const FREEZ = 0,
+  BOIL = 10;
+let x, y;
+
+const notQuitPi = parseFloat("3.14"); // 숫자 3.14
+const evenLessPi = parseInt("3"); // 정수 3
+const notQuitPiString = notQuitPi.toString();
+const evenLessPiString = (3).toString();
