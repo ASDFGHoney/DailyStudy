2ad97c1bef6fba87cd4b9a535e5a117295ebbdec
Chi-Honey <necktop9711@gmail.com>
2022-05-04 23:30:40 +0900

    Daily study 220504

--- WebBasic/JSbasic/functional.js
+++ WebBasic/JSbasic/functional.js
@@ -2,7 +2,7 @@ function average(x, y) {
   return (x + y) / 2;
 }
 
-const stats = (x,y) => ({
-  average: (x+y)/2,
-  distance: Math.abs(x-y)
-})
+const stats = (x, y) => ({
+  average: (x + y) / 2,
+  distance: Math.abs(x - y),
+});
