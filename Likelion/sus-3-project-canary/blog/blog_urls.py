fba62c4949a6b86e65769d3019499260a703c4a2
Chi-Honey <necktop9711@gmail.com>
2022-07-04 10:12:53 +0900

    Daily study 220704

---
+++ Likelion/sus-3-project-canary/blog/blog_urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from blog import views
+
+
+urlpatterns = [
+    path("home/", views.blog_home),
+    path("create/", views.blog_post_create),
+    path("read/", views.blog_post_read),
+    path("update/", views.blog_post_update),
+    path("delete/", views.blog_post_delete),
+]
